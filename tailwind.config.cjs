246a856ba122c50f13158c86d07e5f5d158340d7
Kai Fletcher <kaifletcher@gmail.com>
2022-11-08 11:34:27 +0000

    fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./courseinfo.html",
+    "./basiccourse.html",
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./src/assets",
